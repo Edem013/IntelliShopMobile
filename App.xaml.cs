f04a6d3cfcc80485b122c9d028e75f1832bac272
Laszlo Kiss <kisslac1988@hotmail.com>
2012-09-18 07:23:46 +0200

    Failed Commit

--- App.xaml.cs
+++ App.xaml.cs
@@ -116,8 +116,7 @@ namespace IntelliShopMobile
 
             // Create the frame but don't set it as RootVisual yet; this allows the splash
             // screen to remain active until the application is ready to render.
-//            RootFrame = new PhoneApplicationFrame();
-            RootFrame = new TransitionFrame();
+            RootFrame = new PhoneApplicationFrame();
             RootFrame.Navigated += CompleteInitializePhoneApplication;
 
             // Handle navigation failures
