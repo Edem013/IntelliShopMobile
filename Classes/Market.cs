5805d056401f9ca434b448ff49ec01ef3fc77869
Laszlo Kiss <kisslac1988@hotmail.com>
2012-09-20 18:37:58 +0200

    Pages and Classes were added.

---
+++ Classes/Market.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace IntelliShopMobile.Classes
+{
+    public class Market
+    {
+        private string pmarketName {get; set;}
+        private int pmarketID { get; set; }
+        private string pmarketLocation { get; set; }
+        private List<List> pLists { get; set; }
+
+
+    }
+}
